54009ce41449e3ca043a1b45d73963411d78fd0d
Your Name <you@example.com>
2025-09-21 00:23:04 +0100

    Add changes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,18 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
-    <Router>
+    <div style={{ padding: "20px" }}>
+      <h1>Recipe Sharing App</h1>
+
+      <nav style={{ marginBottom: "20px" }}>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/add">Add Recipe</Link>
+      </nav>
+
+      <SearchBar />
+
       <Routes>
         <Route path="/" element={<RecipeList />} />
+        <Route path="/add" element={<AddRecipeForm />} />
         <Route path="/recipe/:id" element={<RecipeDetails />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
 
 export default App;
-
 
