267c969d4b9bee3a4cab44ab1d8108691d7cc94e
Your Name <you@example.com>
2025-10-15 21:43:34 +0100

    Todolist form project

---
+++ react-todo/src/_tests_/TodoList.test.js
@@ -0,0 +1,33 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import TodoList from "../components/TodoList";
+
+test("renders initial todos", () => {
+  render(<TodoList />);
+  expect(screen.getByText(/Learn React/i)).toBeInTheDocument();
+  expect(screen.getByText(/Build a Todo App/i)).toBeInTheDocument();
+});
+
+test("adds a new todo", () => {
+  render(<TodoList />);
+  const input = screen.getByPlaceholderText(/Add a new todo/i);
+  const button = screen.getByText(/Add/i);
+
+  fireEvent.change(input, { target: { value: "Write Tests" } });
+  fireEvent.click(button);
+
+  expect(screen.getByText(/Write Tests/i)).toBeInTheDocument();
+});
+
+test("toggles todo completion", () => {
+  render(<TodoList />);
+  const todo = screen.getByText(/Learn React/i);
+  fireEvent.click(todo);
+  expect(todo).toHaveStyle("text-decoration: line-through");
+});
+
+test("deletes a todo", () => {
+  render(<TodoList />);
+  const deleteButton = screen.getAllByText(/Delete/i)[0];
+  fireEvent.click(deleteButton);
+  expect(screen.queryByText(/Learn React/i)).not.toBeInTheDocument();
+});
